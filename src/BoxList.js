b294db5ee7cedefceb5c167c21a37b3aada66271
Matheo <mflorez3@live.com>
2021-02-04 15:56:22 -0500

    First Commit

---
+++ src/BoxList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Box from "./Box";
+import NewBoxForm from "./NewBoxForm";
+
+class BoxList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {boxes: [{width: 10, height: 40, color: "orange"}]}
+        this.create = this.create.bind(this)
+    }
+    create(newBox) {
+        this.setState({
+            boxes: [...this.state.boxes, newBox]
+        })
+    }
+
+    render() {
+        const boxes = this.state.boxes.map(box => (
+            <Box
+            width={box.width}
+            height={box.height}
+            color={box.color}
+            />
+        ))
+        return (
+            <div>
+                <h1>Color box maker thingy</h1>
+                <NewBoxForm createBox={this.create} />
+                {boxes}
+            </div>
+        )
+    }
+}
+
+export default BoxList;
